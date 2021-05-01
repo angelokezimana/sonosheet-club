090ce78878f15b623bd9f8299809741207077f7e
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-05-01 10:50:26 +0200

    Add models - Create model Album - Create model Artist - Create model Contact - Create model Booking

--- store/models.py
+++ store/models.py
@@ -1,16 +1,26 @@
-# from django.db import models
-
-ARTISTS = {
-    'francis-cabrel': {'name': 'Francis Cabrel'},
-    'lej': {'name': 'Elijay'},
-    'rosana': {'name': 'Rosana'},
-    'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+from django.db import models
 
 
-ALBUMS = [
-    {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-    {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-    {'name': 'Luna Nueva', 'artists': [
-        ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    reference = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+    artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
+    album = models.OneToOneField(Album, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import ALBUMS
+# from .models import ALBUMS
 
 
 def index(request):
