0a28a6d4b6a9df89d791f02048d121c21aa2ac39
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-05-01 21:16:10 +0200

    Give each url a name

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,9 @@ from django.urls import re_path
 from . import views  # import views so we can use them in urls.
 
 
+app_name = 'store'
 urlpatterns = [
-    re_path(r'^$', views.listing),
-    re_path(r'^(?P<album_id>\d+)$', views.detail),
-    re_path(r'^search/$', views.search),
+    re_path(r'^$', views.listing, name='listing'),
+    re_path(r'^(?P<album_id>\d+)$', views.detail, name='detail'),
+    re_path(r'^search/$', views.search, name='search'),
 ]
