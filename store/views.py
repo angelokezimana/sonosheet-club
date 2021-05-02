9ddc4db512474742bb20b64c10386b1849ac948d
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-05-03 00:48:50 +0200

    Implement the album reservation feature

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db import transaction, IntegrityError
 from django.shortcuts import get_object_or_404, render
 
+from .forms import ContactForm, ParagraphErrorList
 from .models import Album, Artist, Booking, Contact
 
 
@@ -26,7 +28,47 @@ def listing(request):
 
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'store/detail.html', {'album': album})
+    context = {
+        'album': album
+    }
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, error_class=ParagraphErrorList)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+
+            try:
+                with transaction.atomic():
+                    contact = Contact.objects.filter(email=email)
+
+                    if not contact.exists():
+                        contact = Contact.objects.create(
+                            name=name,
+                            email=email
+                        )
+                    else:
+                        contact = contact.first()
+
+                    booking = Booking.objects.create(
+                        contact=contact,
+                        album=album
+                    )
+                    album.available = False
+                    album.save()
+
+                    return render(request, 'store/thanks.html', {'album': album})
+
+            except IntegrityError:
+                form.errors['internal'] = 'Une erreur interne est apparue. Merci de recommencer votre requête.'
+
+    else:
+        form = ContactForm()
+
+    context['form'] = form
+    context['errors'] = form.errors.items()
+    return render(request, 'store/detail.html', context)
 
 
 def search(request):
