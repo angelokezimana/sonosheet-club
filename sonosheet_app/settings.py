84260ffc272c3cbf1509376c5bd2501476b53ba7
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-05-07 15:16:52 +0200

    Customize the admin title

--- sonosheet_app/settings.py
+++ sonosheet_app/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'sonosheet_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
